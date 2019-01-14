eed922c3ea2211973eb2a963cd17a8d399812b2f
M. Eric Irrgang <ericirrgang@gmail.com>
2019-01-14 18:34:50 +0300

    Placeholder packages for PyPI.

---
+++ packaging/gmxapi/gmxapi/__init__.py
@@ -0,0 +1,14 @@
+"""GROMACS Python interface.
+
+    import gmxapi as gmx
+    #gmx.run()
+    # This is a placeholder package...
+
+Entry points
+------------
+
+This package owns the entry point group ``gmxapi``.
+See https://packaging.python.org/specifications/entry-points/
+"""
+
+__version__ = '0.0.0'
